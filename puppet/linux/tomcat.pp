4bc31cfab00839531281b6c61102c508240b3627
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-27 06:50:08 +1000

    Fixed engine xml node

--- puppet/linux/tomcat.pp
+++ puppet/linux/tomcat.pp
@@ -15,7 +15,7 @@ package { 'tomcat9':
 -> file_line { 'Define Tomcat worker name':
   path    => '/etc/tomcat9/server.xml',
   line    => '    <Engine defaultHost="localhost" name="Catalina" jvmRoute="worker1">',
-  match   => '\s*<Engine defaultHost="localhost" name="Catalina">',
+  match   => '\s*<Engine name="Catalina" defaultHost="localhost">',
   replace => true,
   notify  => Service['tomcat9']
 }
