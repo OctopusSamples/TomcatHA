47e4d5ef1de746f6112e5fd371ca452af14198ce
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-30 08:44:18 +1000

    Added links to docs

--- puppet/linux/tentacle.pp
+++ puppet/linux/tentacle.pp
@@ -1,5 +1,8 @@
 include apt
 
+# Get the key with:
+# gpg --show-keys public.key
+# https://security.stackexchange.com/a/199892
 apt::key { 'octopus-repository':
   id     => 'DF4235AA125D66D3938B0377896C79DABCE1F8D1',
   source => 'https://apt.octopus.com/public.key',
