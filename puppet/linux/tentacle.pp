8c7a776a3364f087133c0562cb74ee93c8485c14
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-30 08:42:25 +1000

    Installing the tentacle via puppet

---
+++ puppet/linux/tentacle.pp
@@ -0,0 +1,22 @@
+include apt
+
+apt::key { 'octopus-repository':
+  id     => 'DF4235AA125D66D3938B0377896C79DABCE1F8D1',
+  source => 'https://apt.octopus.com/public.key',
+  server  => 'pgp.mit.edu'
+}
+-> apt::source { 'octopus':
+  comment  => 'This is the Octopus repository',
+  location => 'https://apt.octopus.com/',
+  release  => '',
+  repos    => 'stretch main',
+  key      => {
+    'id' => 'DF4235AA125D66D3938B0377896C79DABCE1F8D1',
+  },
+  include  => {
+    'deb' => true,
+  },
+}
+-> package { 'tentacle':
+  ensure => installed,
+}
