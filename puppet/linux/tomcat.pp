5111db0c70dab7658a0658a5f7f7f238250b4966
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-30 07:54:18 +1000

    Adding session replication database info

--- puppet/linux/tomcat.pp
+++ puppet/linux/tomcat.pp
@@ -26,6 +26,29 @@ package { 'tomcat9':
   replace => true,
   notify  => Service['tomcat9']
 }
+-> archive { '/var/lib/tomcat9/lib/postgresql-42.2.11.jar':
+  ensure         => present,
+  extract        => false,
+  source         => 'https://jdbc.postgresql.org/download/postgresql-42.2.11.jar',
+  allow_insecure => true
+}
+-> file_line { 'Configure session replication':
+  path    => '/etc/tomcat9/context.xml',
+  match   => '</Context>',
+  replace => true,
+  notify  => Service['tomcat9'],
+  line    => @("EOT")
+    <Manager className="org.apache.catalina.session.PersistentManager" distributable="true"  processExpiresFrequency="3" maxIdleBackup="1" >
+        <Store className="org.apache.catalina.session.JDBCStore"
+            driverName="org.postgresql.Driver"
+            connectionURL="jdbc:postgresql://$postgres_server:5432;ConnectionRetryCount=10;ConnectionRetryDelay=6;DatabaseName=tomcat"
+            connectionName="$postgres_user" connectionPassword="$postgres_password"
+            sessionAppCol="app_name" sessionDataCol="session_data" sessionIdCol="session_id"
+            sessionLastAccessedCol="last_access" sessionMaxInactiveCol="max_inactive"
+            sessionTable="session.tomcat_sessions" sessionValidCol="valid_session" />
+    </Manager>
+  | EOT
+}
 
 service {'tomcat9':
   ensure => running
