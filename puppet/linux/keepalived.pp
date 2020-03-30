c9dd8d351e0dbc87ba9b7af953ad530adcf39034
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-30 12:50:47 +1000

    Started work on Keepalive configuration

---
+++ puppet/linux/keepalived.pp
@@ -0,0 +1,34 @@
+package { 'keepalived':
+  ensure => installed
+}
+-> file { '/etc/keepalived/keepalived.conf':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  notify  => Service['keepalived'],
+  content => @("EOT")
+    vrrp_instance $loadbalancer_name {
+        state MASTER
+        interface eth0
+        virtual_router_id 101
+        priority $loadbalancer_priority
+        advert_int 1
+        authentication {
+            auth_type PASS
+            auth_pass $keepalive_pass
+        }
+        virtual_ipaddress {
+            10.0.0.30
+        }
+        unicast_src_ip $loadbalancer_ip
+        unicast_peer {
+          $other_loadbalancer_ip
+        }
+    }
+    | EOT
+}
+
+service {'keepalived':
+  ensure => running
+}
