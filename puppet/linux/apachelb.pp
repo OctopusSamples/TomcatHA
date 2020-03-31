aa7294d53cf0863361716a2632144d05ddc14712
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-31 13:27:55 +1000

    moved https config to a new file

--- puppet/linux/apachelb.pp
+++ puppet/linux/apachelb.pp
@@ -3,10 +3,6 @@ $apache_server = $::operatingsystem ? {
   default => 'httpd',
 }
 
-package { 'augeas-tools':
-  ensure => installed
-}
-
 package { $apache_server:
   ensure => installed
 }
@@ -50,31 +46,6 @@ package { $apache_server:
       CustomLog ${APACHE_LOG_DIR}/access.log combined
       JkMount /* loadbalancer
     </VirtualHost>
-    | EOT
-}
-
-file { '/opt/augapache':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
-  content => @(EOT)
-    #!/usr/bin/augtool -Af
-    set /augeas/load/Httpd/lens Httpd.lns
-    set /augeas/load/Httpd/incl /etc/apache2/sites-enabled/000-default.conf
-    load
-    print /files/etc/apache2/sites-enabled/000-default.conf
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/arg "*:443"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLEngine"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLEngine"]/arg "on"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateFile"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateFile"]/arg "/path/to/octopus_tech.crt"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateKeyFile"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateKeyFile"]/arg "/path/to/octopus_tech.key"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateKeyFile"
-    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateChainFile"]/arg "/path/to/octopus_tech_bundle.pem"
-    print /files/etc/apache2/sites-enabled/000-default.conf
-    save
     | EOT
 }
 
