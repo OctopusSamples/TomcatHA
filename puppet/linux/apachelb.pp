4e73fd7110a585e198387a20bfbc53e53832736c
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-31 13:24:33 +1000

    Changed apache virtualserver to use https

--- puppet/linux/apachelb.pp
+++ puppet/linux/apachelb.pp
@@ -49,10 +49,6 @@ package { $apache_server:
       ErrorLog ${APACHE_LOG_DIR}/error.log
       CustomLog ${APACHE_LOG_DIR}/access.log combined
       JkMount /* loadbalancer
-      SSLEngine on
-      SSLCertificateFile /path/to/octopus_tech.crt
-      SSLCertificateKeyFile /path/to/octopus_tech.key
-      SSLCertificateChainFile /path/to/octopus_tech_bundle.pem
     </VirtualHost>
     | EOT
 }
@@ -68,8 +64,15 @@ file { '/opt/augapache':
     set /augeas/load/Httpd/incl /etc/apache2/sites-enabled/000-default.conf
     load
     print /files/etc/apache2/sites-enabled/000-default.conf
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/arg "*:443"
     set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLEngine"
     set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLEngine"]/arg "on"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateFile"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateFile"]/arg "/path/to/octopus_tech.crt"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateKeyFile"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateKeyFile"]/arg "/path/to/octopus_tech.key"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/directive[last()+1] "SSLCertificateKeyFile"
+    set /files/etc/apache2/sites-enabled/000-default.conf/VirtualHost/*[self::directive="SSLCertificateChainFile"]/arg "/path/to/octopus_tech_bundle.pem"
     print /files/etc/apache2/sites-enabled/000-default.conf
     save
     | EOT
