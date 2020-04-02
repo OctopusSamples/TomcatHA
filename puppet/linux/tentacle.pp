285de6697a5003f5a4b4b0a24871c99afc1db9a5
Matthew Casperson <matthewcasperson@gmail.com>
2020-04-03 07:12:38 +1000

    Registered the tentacles automatically

--- puppet/linux/tentacle.pp
+++ puppet/linux/tentacle.pp
@@ -23,23 +23,23 @@ apt::key { 'octopus-repository':
 -> package { 'tentacle':
   ensure => installed,
 }
-# -> file { '/root/addtentacle.sh':
-#   ensure  => 'file',
-#   owner   => 'root',
-#   group   => 'root',
-#   mode    => '0755',
-#   content => @("EOT")
-#     sudo /opt/octopus/tentacle/Tentacle create-instance --instance "Tentacle" --config "/etc/octopus/Tentacle/tentacle-Tentacle.config"
-#     sudo /opt/octopus/tentacle/Tentacle new-certificate --instance "Tentacle" --if-blank
-#     sudo /opt/octopus/tentacle/Tentacle configure --instance "Tentacle" --app "/home/Octopus/Applications" --noListen "True" --reset-trust
-#     sudo /opt/octopus/tentacle/Tentacle register-with --instance "Tentacle" --server "$octopus_server" --name "$tomcat_name" --comms-style "TentacleActive" --server-comms-port "10943" --apiKey "$octopus_api_key" --space "$octopus_space" --environment "$octopus_environment"  --role "tomcat"
-#     sudo /opt/octopus/tentacle/Tentacle service --install --start --instance "Tentacle"
-#     | EOT
-# }
-# -> exec { 'Add Tentacle':
-#   command   => "/root/addtentacle.sh",
-#   logoutput => true
-# }
+-> file { '/root/addtentacle.sh':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => @("EOT")
+    sudo /opt/octopus/tentacle/Tentacle create-instance --instance "Tentacle" --config "/etc/octopus/Tentacle/tentacle-Tentacle.config"
+    sudo /opt/octopus/tentacle/Tentacle new-certificate --instance "Tentacle" --if-blank
+    sudo /opt/octopus/tentacle/Tentacle configure --instance "Tentacle" --app "/home/Octopus/Applications" --noListen "True" --reset-trust
+    sudo /opt/octopus/tentacle/Tentacle register-with --force --instance "Tentacle" --server "$octopus_server" --name "$tentacle_name" --comms-style "TentacleActive" --server-comms-port "10943" --apiKey "$octopus_api_key" --space "$octopus_space" --environment "$octopus_environment" --role "$octopus_role"
+    sudo /opt/octopus/tentacle/Tentacle service --install --start --instance "Tentacle"
+    | EOT
+}
+-> exec { 'Add Tentacle':
+  command   => "/root/addtentacle.sh",
+  logoutput => true
+}
 
 file { '/root/addpwsh.sh':
   ensure  => 'file',
